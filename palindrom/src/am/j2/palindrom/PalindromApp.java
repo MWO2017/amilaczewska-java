1846735e4efb8a767d801947c0ef86b07db94e0e
Olimp Bockowski <obockows@redhat.com>
2017-12-20 16:27:41 +0100

    The check() method doesn't work properly
    If an example string includes dash sign, then we have a problem.
    One example was added to proof we have the bug.

--- palindrom/src/am/j2/palindrom/PalindromApp.java
+++ palindrom/src/am/j2/palindrom/PalindromApp.java
@@ -5,11 +5,16 @@ public class PalindromApp {
 	public static void main(String[] args) {
 		Palindrom text1 = new Palindrom("Kajak");
 		Palindrom text2 = new Palindrom("kajak !?");
-		String txt="Iwono, ty rabuj okop spokoju barytonowi";
+		String txt = "Iwono, ty rabuj okop spokoju barytonowi";
 		Palindrom text3 = new Palindrom(txt);
-		System.out.println(text1.getTextToCheck()+" is palindrom?: "+text1.check());
-		System.out.println(text2.getTextToCheck()+" is palindrom?: "+text2.check());
-		System.out.println(text3.getTextToCheck()+" is palindrom?: "+text3.check());
+		// bug w przypadku zastosowania znaku -
+		String txt2 = "ale ma palac - oto cala Pamela";
+		Palindrom text4 = new Palindrom(txt2);
+		System.out.println(text1.getTextToCheck() + " is palindrom?: " + text1.check());
+		System.out.println(text2.getTextToCheck() + " is palindrom?: " + text2.check());
+		System.out.println(text3.getTextToCheck() + " is palindrom?: " + text3.check());
+		// zwraca false
+		System.out.println(text4.getTextToCheck() + " is palindrom?: " + text4.check());
 	}
 
 }
